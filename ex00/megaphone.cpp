c926eada1841b3663049a5c31bebdaeff1944d9b
Your Name <a.mura27400@gmail.com>
2025-05-14 13:09:17 +0200

    refactor: use a reusable function

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,13 +2,20 @@
 #include <cstring>
 #include <cctype>
 
+std::string	str_to_upper(std::string s)
+{
+	std::string	s_cpy;
+
+	s_cpy = s;
+	for(std::size_t i = 0; i < s_cpy.size(); i++)
+		s_cpy[i] = (char)std::toupper(s[i]);
+	return (s_cpy);
+}
+
 int main(int argc, char const *argv[])
 {
 	if (argc == 1) std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (std::size_t i = 1; i < (std::size_t)argc; i++) {
-		for (std::size_t j = 0; j < strlen(argv[i]); j++) {
-			std::cout << (char)std::toupper(argv[i][j]);
-		}
-	}
+	for (std::size_t i = 1; i < (std::size_t)argc; i++)
+		std::cout << str_to_upper(argv[i]);
 	return (std::cout << std::endl, 0);
 }
